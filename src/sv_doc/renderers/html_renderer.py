fb6b347c1a4952f21320284015f481d429202d25
Yegor Popov <popov.egor.dev@gmail.com>
2026-02-09 19:21:43 +0300

    Add first templates and html_renderer

--- src/sv_doc/renderers/html_renderer.py
+++ src/sv_doc/renderers/html_renderer.py
@@ -1,11 +1,20 @@
-"""HTML rendering strategy placeholder."""
+"""HTML rendering strategy."""
 
-from sv_doc.interfaces import Renderer
+from jinja2 import Environment, FileSystemLoader
+
 from sv_doc.models.document import Documentation
 
 
-class HtmlRenderer(Renderer):
-    """Render documentation to HTML (placeholder)."""
+class HtmlRenderer:
+    """Render documentation to HTML."""
+
+    file_loader = FileSystemLoader("./templates")
+    env = Environment(loader=file_loader)
 
     def render(self, documentation: Documentation) -> str:
-        raise NotImplementedError
+        data = {
+            "title": "Documentation SystemVerilog",
+            "sections": documentation.sections,
+        }
+        template = self.env.get_template("index.html")
+        return template.render(data)
